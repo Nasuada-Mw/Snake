de27a637fd54497f2946060503122d67018cff8c
Ornella <nellamulume@gmail.com>
2021-06-26 01:51:44 -0700

    Add outline for Snake class and header guards

--- inc/Snake.h
+++ inc/Snake.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef INC_SNAKE_H
+#define INC_SNAKE_H
+
+
+class Snake
+{
+
+    //Members
+    
+    //Border(), Draws the boundary of the snake game
+
+    //Rat(), This function will set the position of the snakes food
+
+    //Input(), Takes input from the keyboard
+
+    //Snake_mvmt(), handles movement of the snake.
+
+};
