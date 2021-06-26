a2040b1cccffc2256598d39b13aca66b4d3034bc
Ornella <nellamulume@gmail.com>
2021-06-26 01:53:01 -0700

    Simple message to test compilation with ncurses and CMake

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -1,10 +1,20 @@
 // Snake.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <stdlib.h>
+#include <ncurses.h>
 
-int main()
+
+int main(void)
 {
+    initscr();
+    printw("Hello World!");
+    refresh();
+    getch();
+    endwin();
+    std::cout << "Built with CMake and Curse!\n";
 
+    return 0;
 }
+
 
